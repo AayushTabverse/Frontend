f60efbb001f35664ad21ef42f042b4ed64015a20
AayushParekh <aayush.parekh@neemandtulsi.com>
2026-03-23 17:46:58 +0000

    env updated

--- menu-app/src/environments/environment.ts
+++ menu-app/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://api.tabverse.in/api',
-  // hubUrl: 'https://api.tabverse.in/hubs/orders'
-  apiUrl: 'https://localhost:7065/api',
-  hubUrl: 'https://localhost:7065/hubs/orders'
+  apiUrl: 'https://api.tabverse.in/api',
+  hubUrl: 'https://api.tabverse.in/hubs/orders'
+  // apiUrl: 'https://localhost:7065/api',
+  // hubUrl: 'https://localhost:7065/hubs/orders'
 };
